95e292d602d1034944be510b3a07f303c8901c27
Oguro Haruka <Oguro Haruka@MSI>
2024-02-15 19:06:49 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -16,6 +16,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	Vector3 from0 = Normalize(Vector3{ 1.0f,0.7f,0.5f });
+	//Vector3 from0 = Normalize(Vector3{ 1.0f,0.7f,0.0f });
+	Vector3 to0 = { -from0.x,-from0.y,-from0.z };
+	Vector3 from1 = Normalize(Vector3{ -0.6f,0.9f,0.2f });
+	Vector3 to1 = Normalize(Vector3{ 0.4f,0.7f,-0.5f });
+	Matrix4x4 rotateMatrix0 = DerectionToDerection(
+	Normalize(Vector3{ 1.0f,0.0f,0.0f }), Normalize(Vector3{ -1.0f,0.0f,0.0f }));
+
+	static const int kWindowWidth = 1280;
+	static const int kWindowHeght = 720;
+	static const int kRowHeight = 20;
+	static const int kColumnWidth = 60;
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -29,7 +42,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 		
-
+		Matrix4x4 rotateMatrix1 = DerectionToDerection(from0, to0);
+		Matrix4x4 rotateMatrix2 = DerectionToDerection(from1, to1);
 
 		///
 		/// ↑更新処理ここまで
@@ -39,7 +53,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-
+		MatrixScreenPrintf(0, 0, rotateMatrix0, "rotateMatrix0");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateMatrix1, "rotateMatrix1");
+		MatrixScreenPrintf(0, kRowHeight * 10, rotateMatrix2, "rotateMatrix2");
 
 		///
 		/// ↑描画処理ここまで
